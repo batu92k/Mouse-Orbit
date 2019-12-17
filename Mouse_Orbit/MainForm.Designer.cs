2df48a1d67283c3b06ab14f9b7b00c8be8c08987
Batuhan Kından <batuhan.kindan@gmail.com>
2019-12-18 00:56:41 +0300

    Default Form Name Changed to MainForm

--- Mouse_Orbit/MainForm.Designer.cs
+++ Mouse_Orbit/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mouse_Orbit
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Mouse Orbit App";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
