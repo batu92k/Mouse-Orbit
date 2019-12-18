e8388c114de3f76949442105a8acc3c764a3b356
Batuhan Kından <batuhan.kindan@gmail.com>
2019-12-18 23:58:23 +0300

    OpenTK GL Control Window Added with Minimal Requirements

--- Mouse_Orbit/MainForm.Designer.cs
+++ Mouse_Orbit/MainForm.Designer.cs
@@ -28,21 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.GL_Monitor = new OpenTK.GLControl();
             this.SuspendLayout();
+            // 
+            // GL_Monitor
+            // 
+            this.GL_Monitor.BackColor = System.Drawing.Color.Black;
+            this.GL_Monitor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GL_Monitor.Location = new System.Drawing.Point(0, 0);
+            this.GL_Monitor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GL_Monitor.Name = "GL_Monitor";
+            this.GL_Monitor.Size = new System.Drawing.Size(782, 553);
+            this.GL_Monitor.TabIndex = 0;
+            this.GL_Monitor.VSync = false;
+            this.GL_Monitor.Load += new System.EventHandler(this.GL_Monitor_Load);
+            this.GL_Monitor.Paint += new System.Windows.Forms.PaintEventHandler(this.GL_Monitor_Paint);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(782, 553);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Controls.Add(this.GL_Monitor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainForm";
             this.Text = "Mouse Orbit App";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private OpenTK.GLControl GL_Monitor;
     }
 }
 
--- Mouse_Orbit/MainForm.cs
+++ Mouse_Orbit/MainForm.cs
@@ -34,14 +34,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
 
 namespace Mouse_Orbit
 {
     public partial class MainForm : Form
     {
+        bool monitorLoaded = false;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void GL_Monitor_Load(object sender, EventArgs e)
+        {
+            monitorLoaded = true;
+            GL.ClearColor(Color.Black);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(Color.Black);
+            GL_Monitor.VSync = false;
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+
+            GL.ClearColor(Color.Black);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.Viewport(GL_Monitor.Size);
+            GL.Ortho(-GL_Monitor.Width / 2, GL_Monitor.Width / 2, -GL_Monitor.Height / 2, GL_Monitor.Height / 2, 20000, -20000);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.ClearDepth(1.0f);
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Lequal);
+        }
+
+        private void GL_Monitor_Paint(object sender, PaintEventArgs e)
+        {
+            if (!monitorLoaded)
+                return;
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+
+            GL.ClearColor(Color.Black);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.Viewport(GL_Monitor.Size);
+            GL.Ortho(-GL_Monitor.Width / 2, GL_Monitor.Width / 2, -GL_Monitor.Height / 2, GL_Monitor.Height / 2, 20000, -20000);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.ClearDepth(1.0f);
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Lequal);
+
+            GL_Monitor.SwapBuffers();
         }
     }
 }
