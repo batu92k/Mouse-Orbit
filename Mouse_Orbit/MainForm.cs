64c15d08a8ff29655927e5b3e93dbd22f4c3d2bb
Batuhan Kından <batuhan.kindan@gmail.com>
2019-12-18 23:39:31 +0300

    MainForm File Information Header Added

--- Mouse_Orbit/MainForm.cs
+++ Mouse_Orbit/MainForm.cs
@@ -1,4 +1,31 @@
-﻿using System;
+﻿/**
+  ******************************************************************************
+  * @file    MainForm.cs
+  * @author  Ali Batuhan KINDAN
+  * @date    17.12.2019
+  * @brief   This program performs the quaternion based mouse orbit algorithm
+  ******************************************************************************
+  */
+
+/*
+ *  Mouse Orbit - Quaternion Based Mouse Orbit Algorithm
+ *  Copyright (C) 2017, 2019  Ali Batuhan KINDAN
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
