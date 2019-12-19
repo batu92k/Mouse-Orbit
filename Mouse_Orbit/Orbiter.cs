f0f4605ebc7fe296dbcd3503c36732c5c2302b6c
Batuhan Kından <batuhan.kindan@gmail.com>
2019-12-20 02:10:44 +0300

    Quat Make Function Defined and Var Names Changed

--- Mouse_Orbit/Orbiter.cs
+++ Mouse_Orbit/Orbiter.cs
@@ -25,22 +25,54 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+using System;
 
 namespace Mouse_Orbit
 {
 
     public class Orbiter
     {
-        private struct quaternion
+        private struct Quaternion
         {
             public float qw, qx, qy, qz;
-            public quaternion(float w = 1, float x = 0, float y = 0, float z = 0)
+            public Quaternion(float w = 1, float x = 0, float y = 0, float z = 0)
             {
                 qw = w;
                 qx = x;
                 qy = y;
                 qz = z;
             }
+        }
+
+        private struct Vector3
+        {
+            public float vx, vy, vz;
+            public Vector3(float x = 1, float y = 1, float z = 1)
+            {
+                vx = x;
+                vy = y;
+                vz = z;
+            }
+        }
+
+        /**
+          * @brief  This function is using to make a rotation quaternion with
+          *         given vector and rotation amount in radians
+          * @param  radTheta
+          * @param  v
+          * @retval Quaternion
+          */
+        private Quaternion Quat_Make(float radTheta, Vector3 v)
+        {
+            Quaternion q = new Quaternion
+            {
+                qw = (float)Math.Cos(radTheta / 2.0f),
+                qx = v.vx * (float)Math.Sin(radTheta / 2.0f),
+                qy = v.vy * (float)Math.Sin(radTheta / 2.0f),
+                qz = v.vz * (float)Math.Sin(radTheta / 2.0f)
+            };
+
+            return q;
         }
 
     }
