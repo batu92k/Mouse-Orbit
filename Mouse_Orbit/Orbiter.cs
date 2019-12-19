0c7dcfabc60418711bd60851a50266d20ebbe914
Batuhan Kından <batuhan.kindan@gmail.com>
2019-12-20 01:39:57 +0300

    Orbiter Source File Created

---
+++ Mouse_Orbit/Orbiter.cs
@@ -0,0 +1,36 @@
+﻿/**
+  ******************************************************************************
+  * @file    Orbiter.cs
+  * @author  Ali Batuhan KINDAN
+  * @date    20.12.2019
+  * @brief   This file contains the classes amd methods for handling the mouse
+  *          orbit, mouse pan and mouse zoom calculations
+  ******************************************************************************
+  */
+
+/*
+ *  Mouse Orbit - Quaternion Based Mouse Orbit Algorithm
+ *  Copyright (C) 2017, 2019  Ali Batuhan KINDAN
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Mouse_Orbit
+{
+
+    public class Orbiter
+    {
+
+    }
+}
