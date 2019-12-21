042ab4a0c77ef9fd3f5d7dbfd133085c9e3be7a1
Batuhan Kından <batuhan.kindan@gmail.com>
2019-12-21 21:43:27 +0300

    Orbit Feature Functions and Variables Implemented

--- Mouse_Orbit/Orbiter.cs
+++ Mouse_Orbit/Orbiter.cs
@@ -32,6 +32,29 @@ namespace Mouse_Orbit
 
     public class Orbiter
     {
+        const double deg2Rad = Math.PI / 180.0;
+        const double rad2Deg = 180.0 / Math.PI;
+        int mouseX_Old = 0;
+        int mouseY_Old = 0;
+        int difX = 0;
+        int difY = 0;
+        Quaternion qdMouse = new Quaternion(1, 0, 0, 0);
+        Quaternion qGlobal_E = new Quaternion(1, 0, 0, 0);
+        Quaternion qGlobal = new Quaternion(1, 0, 0, 0);
+        Vector3 vdMouse = new Vector3(0, 0, 0);
+
+        public struct Orbit
+        {
+            public float angle, ox, oy, oz;
+            public Orbit(float angleDeg = 0, float x = 0, float y = 0, float z = 0)
+            {
+                angle = angleDeg;
+                ox = x;
+                oy = y;
+                oz = z;
+            }
+        }
+
         private struct Quaternion
         {
             public float qw, qx, qy, qz;
@@ -110,6 +133,53 @@ namespace Mouse_Orbit
             };
 
             return q3;
+        }
+
+        /**
+          * @brief  This function is using for calculate the derivative of mouse coordinates.
+          *         Calculated derivative values are using in the orbit and pan features.
+          *         This function needs to be called periodically inside a timer or thread 
+          *         to make orbit and pan features work.
+          * @param  mouseX
+          * @param  mouseY
+          * @retval none
+          */
+        public void Update_Coordinates(int mouseX, int mouseY)
+        {
+            difX = mouseX - mouseX_Old;
+            difY = -(mouseY - mouseY_Old);
+            mouseX_Old = mouseX;
+            mouseY_Old = mouseY;
+        }
+
+        /**
+          * @brief  This function is using to calculate current raotation quaternion from
+          *         mouse coordinates that are updated from Update_Coordinates function.
+          * @param  mouseX
+          * @param  mouseY
+          * @retval Quaternion
+          */
+        public Orbit Get_Orbit()
+        {
+            if(difX != 0 || difY != 0)
+            {
+                /* rotate mouse derivative vector 90 degrees and assign to mouse vector 
+                   in order to assemble the rotation shaft. to rotate 90 degrees CCW we
+                   need to multiply coordinate values with "i". if v = x + yi , v' = i * v 
+                   so v' = -y + xi which is the rotated vector.*/
+                vdMouse.vx = -difY;
+                vdMouse.vy = difX;
+                vdMouse.vz = 0;
+
+                float dTheta = -(float)Math.Sqrt(vdMouse.vx * vdMouse.vx + vdMouse.vy * vdMouse.vy);
+                qdMouse = Quat_Make((float)(dTheta * deg2Rad), vdMouse);
+                Quat_Normalize(ref qdMouse); /* normalize before rotation */
+                qGlobal = Quat_Multiply(qdMouse, qGlobal_E); /* inverse rotation */
+                Quat_Normalize(ref qGlobal); /* normalize after rotation */
+                qGlobal_E = qGlobal; /* update old value to use it in the next iteration */
+            }
+
+            return new Orbit((float)(2 * Math.Acos(qGlobal.qw) * rad2Deg), qGlobal.qx, qGlobal.qy, qGlobal.qz);
         }
 
     }
